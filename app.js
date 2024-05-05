38a01a3d9ea41e14f8fb4321591a6c6c4a9027bf
zahraa-6 <za998916@gmail.com>
2024-05-05 12:01:32 -0700

    push my homework

---
+++ app.js
@@ -0,0 +1,55 @@
+
+
+var yourAge = 18;
+if(yourAge <= 18){
+  console.log('checkVotingEligibility')
+}else{
+  console.log('You are not eligible for voting')
+}
+
+
+  
+// task 2
+let i = 1;
+while (i < 6) {
+    console.log(i);
+    i += 1;
+}
+
+
+// task3
+
+    for (let i = 0; i <= 10; i += 2) {
+      console.log(i);
+    }
+
+
+//   task4
+   let day=1;
+  switch(day){
+    case 1:
+      console.log("sunday");
+    case 2:
+      console.log("monday");
+    case 3:
+      console.log("tuesday");
+    case 4:
+      console.log("wednesday");
+    case 5: 
+      console.log("thursday");
+    case 6: 
+      console.log("friday");
+    case 7:  
+      console.log("saterday");
+  }
+
+
+  // task5
+  function sum(nam1,nam2){
+    console.log(nam1+nam2)
+    }
+    sum(zahraa,ahmed)
+
+  // task6
+  var fruits =["apple" , "banana" ,"orange" , "blueberry"]
+console.log(fruits);
